163042587a19ae5df831d9aef0c8273850e85d09
karthik <karthik.anumula@suraaga.com>
2017-10-30 12:30:49 +0530

    adding a project as trial

---
+++ Dealer_dashboard/app/URL.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ROOT_URL:'http://192.168.94.20:3042',
+    //ROOT_URL:'http://192.168.0.28:3042',
+    OKTA_URL:'http://192.168.94.21:3000',
+   // OKTA_URL:'http://192.168.0.28:3000'
+}
